3433507e99119bfae30c90c8f5cb85aedb0a348d
fishwangx <yu@taifanmagellan.com>
2019-03-25 12:01:02 +0800

    added jasonify

--- full_stack/project.py
+++ full_stack/project.py
@@ -1,7 +1,7 @@
 from database_setup import Base, Restaurant, MenuItem
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from flask import Flask, render_template, request, redirect, url_for , flash
+from flask import Flask, render_template, request, redirect, url_for , flash ,jsonify
 app = Flask(__name__)
 
 
@@ -83,6 +83,16 @@ def deleteMenuItem(restaurant_id, menu_id):
 
     else:
         return render_template('deletemenuitem.html',restaurant_id=restaurant_id,menu_id=menu_id,item=item)
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJson(restaurant_id):
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
 
 
 
